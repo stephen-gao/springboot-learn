912d898f8ffe42cd4ec0f56d42522e6d6f1f319f
gaosheng <gaoshengok@sina.com>
2019-05-11 16:18:25 +0800

    first commit

---
+++ firstdemo/src/main/java/com/gao/web/TestController.java
@@ -0,0 +1,21 @@
+package com.gao.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author gao
+ * @Date created time 2019/5/11 10:45
+ * @Description
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("index/{name}")
+    public String index(@PathVariable String name){
+        return "hi " + name + "!";
+    }
+}
